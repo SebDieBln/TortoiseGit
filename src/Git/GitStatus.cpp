8f7fb1ac75a2dfb9a87d04e8ff8221c2662ea2e7
Sven Strickroth <email@cs-ware.de>
2017-05-29 23:01:41 +0200

    fix

--- src/Git/GitStatus.cpp
+++ src/Git/GitStatus.cpp
@@ -413,7 +413,7 @@ int GitStatus::EnumDirStatus(const CString& gitdir, const CString& subpath, git_
 				dirstatus[onepath] = { git_wc_status_unversioned, git_wc_status_unversioned, false, false };
 		}
 		else if (pos == NPOS && posintree != NPOS) /* check if file delete in index */
-			dirstatus[onepath] = { git_wc_status_none, git_wc_status_missing, false, false };
+			dirstatus[onepath] = { git_wc_status_missing, git_wc_status_none, false, false };
 		else if (pos != NPOS && posintree == NPOS) /* Check if file added */
 		{
 			if ((*indexptr)[pos].m_Flags & GIT_IDXENTRY_STAGEMASK)
